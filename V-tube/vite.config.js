2301d5b563d8ca10f47e7080102808a0fc4443b1
yashsharma6391 <ys7097569@gmail.com>
2025-08-28 21:30:50 +0530

    Update vite.config.js
    second commit

--- V-tube/vite.config.js
+++ V-tube/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
-const BackendUrl = import.meta.env.VITE_BACKEND_URL;
+// const BackendUrl = import.meta.env.VITE_BACKEND_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
     server: {
     proxy: {
-      '/api': `${BackendUrl}`  
+      // '/api': `${BackendUrl}`  
     }
   },
   plugins: [react(), svgr()],
